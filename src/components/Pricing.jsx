52e07d3035213ce6da8b651a4bf153c4bad671c8
Rajarshi38 <rajarshibiswas38@gmail.com>
2021-12-11 21:23:45 +0530

    added tick logo

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {IoCheckmarkCircleSharp} from 'react-icons/io5'
 
 const Pricing = ({type,price,paragraph,benefits}) => {
 
@@ -17,21 +18,32 @@ const Pricing = ({type,price,paragraph,benefits}) => {
                 }}>{type}</div>
             <div style={{maxWidth:"80%"}}>{paragraph}</div>
             <div><h1 style = {
-                hover ? {color : "white"} : {color : "#2b2a2a"}
+                hover ? {transition : "color 0.3s ease-in",color : "white"} : {color : "#2b2a2a"}
             }>${price}</h1></div>
             <div className="benefits">
             {benefits.map((benefit)=>(
-                <div >{benefit}</div>
+                <div><IoCheckmarkCircleSharp style={hover ? {
+                    fontSize : "15px",
+                    color : "white",
+
+
+                }:{
+                    transition : "all 0.3s ease-in-out",
+                    fontSize : "15px",
+                    color : "#1f7abf"
+                    
+                }}/>{benefit}</div>
             ))}
 
             </div>
 
             <button style={
                 hover ? {
+                    transition : "color 0.3s ease-in",
                     backgroundColor : "white",
                     color : "#0d64de",
                     border : "none",
-                    transition : "color 0.3s ease-in"
+                    
                 } : {}
             }>Let's Go</button>
         </div>
